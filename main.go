3739de0178be628412e1cb6c3f0cf13021be1878
Roy Larsen <roy.larsen@hbgusa.com>
2016-07-20 11:08:36 -0400

    Changed reference to my fork

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/justinclayton/packer-builder-vsphere/vsphere"
+	"github.com/roylarsen/packer-builder-vsphere/vsphere"
 	"github.com/mitchellh/packer/packer/plugin"
 	"os"
 )
